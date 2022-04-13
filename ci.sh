beb5c9e2cc6f517a4abc957c5f305d1b5483e35e
Matthew McGowan <mat.mcgowan+git@gmail.com>
2022-04-13 13:41:41 -0700

    do a clean build for good measure.

--- ci.sh
+++ ci.sh
@@ -12,6 +12,6 @@ STM32CUBEIDE_BUILD_TARGET=${STM32CUBEIDE_BUILD_TARGET:-all}
 
 # Run build
 # MDM: I had to remove the `-data .` parameter before import for the build to begin. 
-${STM32CUBEIDE_PATH}/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild  -import . -build ${STM32CUBEIDE_BUILD_TARGET} -no-indexer
+${STM32CUBEIDE_PATH}/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild  -import . -cleanBuild ${STM32CUBEIDE_BUILD_TARGET} -no-indexer
 build_ret=$?
 exit $build_ret
